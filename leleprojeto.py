7d9a06e33ecb6466f8da53aef25e97f74d5580d6
leticialindona <leticiafinocchi@gmail.com>
2026-04-28 15:42:33 -0300

    Update leleprojeto.py

--- leleprojeto.py
+++ leleprojeto.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title('Teste ECMI 2') 
-st.write("Esse é o meu texto")
+st.title('teste da lele') 
+st.write("a lele gosta de unicornios")
 st.image("https://github.com/leticialindona/projeto-da-lele-/blob/main/download.jpg?raw=true")
 nome = st.text_input('Digite o seu nome')
 if nome:
